1a738d6664cfabe75c758097266c1720538b8dbe
Shubham Panchore <149653353+shu-bham03@users.noreply.github.com>
2024-03-01 12:55:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const boxClick = () => {
+  document.getElementById("main").style.overflow = "none";
+  document.getElementById("main").style.height = "250px";
+};
+const box2Click = () => {
+  document.getElementById("mid").style.overflow = "none";
+  document.getElementById("mid").style.height = "250px";
+};
+const box3Click = () => {
+  document.getElementById("last").style.overflow = "none";
+  document.getElementById("last").style.height = "250px";
+};
